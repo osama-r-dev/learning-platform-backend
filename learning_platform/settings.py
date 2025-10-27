4fa22701676491d51e50d020986a5714fbbe47b1
osama-rehaili <osama.rehaili.dev@gmail.com>
2025-10-27 13:34:05 +0300

    creating Employee and Course models

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app'
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 
@@ -48,7 +48,7 @@ CORS_ALLOWED_ORIGINS = [
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+
+    name = models.CharField(max_length= 50)
+    department = models.CharField(max_length= 50)
+   
+
+    
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    skills = models.CharField(max_length=200)
+    date_created = models.DateField(auto_now=True)
+    employee = models.ForeignKey(Employee, on_delete= models.CASCADE, related_name="courses")
